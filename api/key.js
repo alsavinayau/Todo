3aa29f1d2f6239421b89895f3ea04d13e5cbaae5
alsavinayau <166839506+alsavinayau@users.noreply.github.com>
2024-04-19 10:10:55 -0500

    Add files via upload

---
+++ api/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://alsavinay:Buddish1509@cluster0.yqyicvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
